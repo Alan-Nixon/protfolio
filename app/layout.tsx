3f6c5952d876c2b1d791794b8939f89a59af9e98
Alan-Nixon <alannixon2520@gmail.com>
2025-03-13 22:42:58 +0530

    co csite button fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,25 +3,28 @@ import "../mongodbConnect/database";
 import { Inter } from "next/font/google";
 import { layoutProps } from "../interfaces_types/interfaces_types";
 import ClientWrapper from "./ClientProvider";
-import Head from "next/head";
+// import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Alan Nixon",
   description: "Showcase of my work and skills as a developer",
+  verification: {
+    google: "oczS7ejkFQaDtfQbHV2i7ny8A3D10jv3P8MLcwLbEiY", 
+  },
   icons: {
-    icon: "/path-to-your-image/favicon.ico", // Favicon
-    apple: "/path-to-your-image/apple-touch-icon.png", // Apple touch icon
+    icon: "./favicon_io/favicon.ico",
+    apple: "https://res.cloudinary.com/dyh7c1wtm/image/upload/v1729344482/protfolio/ewnsnr13wxpa8ethsaod.jpg",
   },
   openGraph: {
     title: "Alan Nixon - Developer Portfolio",
     description: "Showcase of my work and skills as a developer",
-    url: "https://yourwebsite.com",
+    url: "https://alan-nixon.vercel.app",
     siteName: "Alan Nixon Portfolio",
     images: [
       {
-        url: "https://yourwebsite.com/path-to-image.jpg", // OG Image URL
+        url: "https://res.cloudinary.com/dyh7c1wtm/image/upload/v1729344482/protfolio/ewnsnr13wxpa8ethsaod.jpg",
         width: 1200,
         height: 630,
         alt: "Alan Nixon Portfolio Image",
@@ -34,19 +37,15 @@ export const metadata = {
     card: "summary_large_image",
     title: "Alan Nixon - Developer Portfolio",
     description: "Showcase of my work and skills as a developer",
-    images: ["https://yourwebsite.com/path-to-image.jpg"], // Twitter image
+    images: ["https://res.cloudinary.com/dyh7c1wtm/image/upload/v1729344482/protfolio/ewnsnr13wxpa8ethsaod.jpg"],
   },
 };
+
 
 export default function RootLayout({ children }: layoutProps) {
   return (
     <html lang="en">
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="oczS7ejkFQaDtfQbHV2i7ny8A3D10jv3P8MLcwLbEiY"
-        />
-      </Head>
+      
       <body
         className={`${inter.className} bg-white text-gray-900 min-h-screen flex flex-col`}
       >
--- app/page.tsx
+++ app/page.tsx
@@ -50,13 +50,13 @@ function ProfileSection() {
         clearInterval(intervalId);
       }
     }, 100);
-
     return () => clearInterval(intervalId);
   }, []);
 
   if (!user.name) {
     return <LoadingPage />;
   }
+  console.log(user);
 
   return (
     <section className="py-20 text-center">
@@ -64,7 +64,7 @@ function ProfileSection() {
         <div className="w-56 h-56 mx-auto rounded-full overflow-hidden">
           {user.profileImage && (
             <Image
-              src={user.profileImage}
+              src={user.profileImage || "/placeholder.svg"}
               alt="Your Name"
               width={250}
               height={192}
@@ -77,15 +77,25 @@ function ProfileSection() {
         Hi, I&apos;m <span className="text-emerald-700">{user.name}</span>
       </h1>
       <p className="text-xl mb-8 text-gray-600 reveal">{user.bio}</p>
-      <Link href="../alan_nixon_resume.pdf"
-      className="bg-cyan-700 reveal mr-2 text-white px-6 py-3 rounded-full hover:bg-cyan-800 transition-colors inline-flex items-center"
-      > Download Resume <Download className="ml-2" /> </Link>
-      <Link
-        href="/contact"
-        className="bg-emerald-700 reveal text-white px-6 py-3 rounded-full hover:bg-emerald-800 transition-colors inline-flex items-center"
-      >
-        Get in touch <ArrowRight className="ml-2" />
-      </Link>
+
+      <div className="flex flex-col sm:flex-row justify-center gap-4 sm:gap-6 mb-8">
+        <Link
+          href="../alan_nixon_resume.pdf"
+          className=" w-full sm:w-auto bg-cyan-700 reveal text-white px-6 py-3 rounded-full 
+                   hover:bg-cyan-800 transition-colors flex items-center justify-center"
+        >
+          Download Resume <Download className="ml-2" />
+        </Link>
+
+        <Link
+          href="/contact"
+          className=" w-full sm:w-auto bg-emerald-700 reveal text-white px-6 py-3 rounded-full 
+                   hover:bg-emerald-800 transition-colors flex items-center justify-center"
+        >
+          Get in touch <ArrowRight className="ml-2" />
+        </Link>
+      </div>
+
       <div className="mt-12 max-w-2xl mx-auto">
         <p className="text-gray-700 leading-relaxed reveal animate-fade-in">
           {text}
