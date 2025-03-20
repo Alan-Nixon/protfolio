431b4d86b438855e550fc353510f246f62371e17
Alan-Nixon <alannixon2520@gmail.com>
2025-03-20 23:03:54 +0530

    seo h1 added in loading

--- app/(components)/LoadingPage.tsx
+++ app/(components)/LoadingPage.tsx
@@ -1,6 +1,9 @@
 export default function LoadingPage() {
   return (
     <div className="flex items-center justify-center h-screen">
+      <h1 className="sr-only">
+        Alan Nixon - MERN Stack Developer & Freelancer
+      </h1>
       <div className="relative">
         <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
         <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin"></div>
