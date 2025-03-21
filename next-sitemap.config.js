dc4bc3bd142db3d35ba4c584135aa58acc8a73f9
Alan-Nixon <alannixon2520@gmail.com>
2025-03-21 20:19:00 +0530

    Bing search console fixing 1

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://alan-nixon.vercel.app", // Replace with your domain
+    // siteUrl: "https://alan-nixon.vercel.app", // Replace with your domain
     generateRobotsTxt: true, // Generates robots.txt file
     sitemapSize: 5000,
   };
