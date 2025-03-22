e7e6fa549702ee2ed13443048e86a130b186c264
Alan-Nixon <alannixon2520@gmail.com>
2025-03-22 19:07:34 +0530

    admin model added try to fix

--- mongodbConnect/database.ts
+++ mongodbConnect/database.ts
@@ -1,3 +1,7 @@
+import AdminModel from '@/models/admin'
 import { connect } from 'mongoose'
 
-connect(process.env.MONGO_URI + "").then(() => console.log("Mongodb connected successfully"))
+connect(process.env.MONGO_URI + "").then(() => {
+    AdminModel.find().then(res => console.log(res))
+    console.log("Mongodb connected successfully")
+})
