97530f0e8a53defc21aefacc35e5d279fc942388
Alan-Nixon <alannixon2520@gmail.com>
2025-03-22 18:44:29 +0530

    admin model have issues

--- app/api/admin/login/route.ts
+++ app/api/admin/login/route.ts
@@ -1,28 +1,24 @@
-// import { compare } from "bcrypt"
-import AdminModel from '@/models/admin'
+import { compare } from "bcrypt"
 
+const data = {
+    // _id: new ObjectId('67d56157d1af9ce2edf81d39'),
+    Email: 'alannixon2520@gmail.com',
+    Password: '$2b$10$fYsZTOQWFmI7mMlR98uqWuLwsaOvxOiz6gt0HIqvGFHES66ZDiHZW'
+}
 export const POST = async (req: Request) => {
     try {
 
         const { Email, Password } = await req.json();
-        const [data] = await AdminModel.find({ Email:Email });
-        console.log(data);
-        // if (!data) { return new Response("User not found", { status: 400 }) }
 
-        // if (data.Email !== Email) {
-        //     const res = JSON.stringify({ status: false, message: "Email doesn't match" })
-        //     return new Response(res, { status: 200 });
-        // }
-        // if (!await compare(Password, data.Password)) {
-        //     const res = JSON.stringify({ status: false, message: "Incorrect Password try again" })
-        //     return new Response(res, { status: 200 });
-        // }
-        console.log(Email, Password);
-        // const data = {
-        //     // _id: new ObjectId('67d56157d1af9ce2edf81d39'),
-        //     Email: 'alannixon2520@gmail.com',
-        //     Password: '$2b$10$fYsZTOQWFmI7mMlR98uqWuLwsaOvxOiz6gt0HIqvGFHES66ZDiHZW'
-        //   }
+        if (data.Email !== Email) {
+            const res = JSON.stringify({ status: false, message: "Email doesn't match" })
+            return new Response(res, { status: 200 });
+        }
+        if (!await compare(Password, data.Password)) {
+            const res = JSON.stringify({ status: false, message: "Incorrect Password try again" })
+            return new Response(res, { status: 200 });
+        }
+
         const res = JSON.stringify({ status: true, message: "success", data })
         return new Response(res, { status: 200 });
     } catch (error) {
